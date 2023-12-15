c6a5ed752ad5e62c86555cdefd53fe7b2632ee66
KMarcjanowicz <51883166+KMarcjanowicz@users.noreply.github.com>
2023-12-15 14:28:23 +0100

    Add Task form
    - add tasks based on component state
    - func value handlers

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { ChangeEvent, SetStateAction } from 'react'
+import { useState } from 'react'
 
 const AddTask = () => {
+
+  const [text, setText] = useState<string>('');
+  const [day, setDay] = useState<string>('');
+  const [reminder, setReminder] = useState<boolean>(false);
+
+  const handleChangeString = (event: ChangeEvent<HTMLInputElement>, type:string, value: | SetStateAction<string>) => {
+    if(type === 'task'){
+      setText(value);
+    }
+    else if(type === 'day'){
+      setDay(value);
+    }
+  }
+
+  const handleChangeBoolean = (event: ChangeEvent<HTMLInputElement>, type:string, value: | SetStateAction<boolean>) => {
+    if(type === 'reminder'){
+      setReminder(value);
+    }
+  }
+
   return (
     <form className='add-form'>
 
       <div className='form-control'>
         <label>Task:</label>
-        <input type='text' placeholder='Add task'/>
+        <input type='text' placeholder='Add task' value={text} onChange={(e) => handleChangeString(e, 'task', e.target.value)}/>
       </div>
 
       <div className='form-control'>
         <label>Day & Time:</label>
-        <input type='text' placeholder='Add day & time'/>
+        <input type='text' placeholder='Add day & time' value={day} onChange={(e) => handleChangeString(e, 'day', e.target.value)}/>
       </div>
 
       <div className='form-control form-control-check'>
         <label>Set Reminder</label>
-        <input type='checkbox'/>
+        <input type='checkbox' checked={reminder} onChange={(e) => handleChangeBoolean(e, 'reminder', e.target.checked)}/>
       </div>
       
       <input type='submit' value='Save Task' className='btn btn-block'/>
